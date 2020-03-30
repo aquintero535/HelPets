0c962ebf6e599e9654180a35e8863893572cc60a
Adrian Quintero <adrianquintero61@gmail.com>
2020-03-30 11:49:01 -0500

    Añadido Fragment de menú y lista para adoptar mascotas

---
+++ app/src/main/java/com/example/helpets/UI/ActivityAdopta.java
@@ -0,0 +1,14 @@
+package com.example.helpets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAdopta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adopta);
+    }
+}
