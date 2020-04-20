a02b2dd7b6969ea7c5944a0bf4c0efe6348bf5d3
Adrian Quintero <adrianquintero61@gmail.com>
2020-04-20 15:14:57 -0500

    Código optimizado en ActivityWikipetsInfo.

--- app/src/main/java/com/example/helpets/UI/Wikipets/ActivityWikipetsInfo.java
+++ app/src/main/java/com/example/helpets/UI/Wikipets/ActivityWikipetsInfo.java
@@ -17,8 +17,6 @@ public class ActivityWikipetsInfo extends AppCompatActivity implements View.OnCl
     private TextView textoInfoTitulo;
     private TextView textoInfoCuerpo;
 
-    private int pregunta;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,63 +26,25 @@ public class ActivityWikipetsInfo extends AppCompatActivity implements View.OnCl
         imagenInfo = (ImageView)findViewById(R.id.imagenInformacion);
         textoInfoTitulo = (TextView)findViewById(R.id.textoInformacionTitulo);
         textoInfoCuerpo = (TextView)findViewById(R.id.textoInformacionCuerpo);
-        pregunta = getIntent().getIntExtra("pregunta", 0);
         llenarInfo();
     }
 
+    //Llena la información según la pregunta escogida por el usuario. 
     private void llenarInfo(){
-        switch (pregunta){
-            case 0:
-                imagenInfo.setImageResource(R.drawable.pregunta0_imagen);
-                textoInfoTitulo.setText(R.string.pregunta0);
-                textoInfoCuerpo.setText(R.string.pregunta0Info);
-                break;
-            case 1:
-                imagenInfo.setImageResource(R.drawable.pregunta1_imagen);
-                textoInfoTitulo.setText(R.string.pregunta1);
-                textoInfoCuerpo.setText(R.string.pregunta1Info);
-                break;
-            case 2:
-                imagenInfo.setImageResource(R.drawable.pregunta2_imagen);
-                textoInfoTitulo.setText(R.string.pregunta2);
-                textoInfoCuerpo.setText(R.string.pregunta2Info);
-                break;
-            case 3:
-                imagenInfo.setImageResource(R.drawable.pregunta3_imagen);
-                textoInfoTitulo.setText(R.string.pregunta3);
-                textoInfoCuerpo.setText(R.string.pregunta3Info);
-                break;
-            case 4:
-                imagenInfo.setImageResource(R.drawable.pregunta4_imagen);
-                textoInfoTitulo.setText(R.string.pregunta4);
-                textoInfoCuerpo.setText(R.string.pregunta4Info);
-                break;
-            case 5:
-                imagenInfo.setImageResource(R.drawable.pregunta5_imagen);
-                textoInfoTitulo.setText(R.string.pregunta5);
-                textoInfoCuerpo.setText(R.string.pregunta5Info);
-                break;
-            case 6:
-                imagenInfo.setImageResource(R.drawable.pregunta6_imagen);
-                textoInfoTitulo.setText(R.string.pregunta6);
-                textoInfoCuerpo.setText(R.string.pregunta6Info);
-                break;
-            case 7:
-                imagenInfo.setImageResource(R.drawable.pregunta7_imagen);
-                textoInfoTitulo.setText(R.string.pregunta7);
-                textoInfoCuerpo.setText(R.string.pregunta7Info);
-                break;
-            case 8:
-                imagenInfo.setImageResource(R.drawable.pregunta8_imagen);
-                textoInfoTitulo.setText(R.string.pregunta8);
-                textoInfoCuerpo.setText(R.string.pregunta8Info);
-                break;
-            case 9:
-                imagenInfo.setImageResource(R.drawable.pregunta9_imagen);
-                textoInfoTitulo.setText(R.string.pregunta9);
-                textoInfoCuerpo.setText(R.string.pregunta9Info);
-                break;
-        }
+        //Obtengo el número de pregunta.
+        int preguntaId = getIntent().getIntExtra("pregunta", 0);
+
+        String url = "drawable/"+"pregunta"+ preguntaId +"_imagen"; //Dirección de la imagen.
+        int imagenId = getResources().getIdentifier(url, "drawable", getPackageName());
+        url = "pregunta"+ preguntaId; //Dirección del string que contiene el título.
+        int tituloId = getResources().getIdentifier(url, "string", getPackageName());
+        url = url.concat("Info"); //Dirección del string que contiene el cuerpo.
+        int cuerpoId = getResources().getIdentifier(url, "string", getPackageName());
+
+        //Añado la imagen y string a las View.
+        imagenInfo.setImageResource(imagenId);
+        textoInfoTitulo.setText(tituloId);
+        textoInfoCuerpo.setText(cuerpoId);
     }
 
     @Override
